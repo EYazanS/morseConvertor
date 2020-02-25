735ea210d192faa9b26dca884a95d2cac88f6311
Yazan Salameh <e.yazan@live.com>
2020-02-26 00:05:47 +0200

    Implement dictionary morse code

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MorseConverter
 {
@@ -6,7 +7,29 @@ namespace MorseConverter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            foreach (var argument in args)
+            {
+                foreach (var letter in argument)
+                {
+                    Console.Write(ConvertLetterToMorseCode(letter));
+                }
+            }
         }
+
+        static string ConvertLetterToMorseCode(char letter)
+        {
+            return MorseTable[char.ToLower(letter)] + " ";
+        }
+
+        static IDictionary<char, string> MorseTable = new Dictionary<char, string>
+        {
+            { 'a', ".-" }, { 'b', "-..." }, { 'c', "-.-." }, { 'd', "-.." },
+            { 'e', "." }, { 'f', "..-." }, { 'g', "--." }, { 'h', "...." },
+            { 'i', ".." }, { 'j', ".---" }, { 'k', "-.-" }, { 'l', ".-.." },
+            { 'm', "--" }, { 'n', "-." }, { 'o', "---" }, { 'p', ".--." },
+            { 'q', "--.-" }, { 'r', ".-." }, { 's', "..." }, { 't', "-" },
+            { 'u', "..-" }, { 'v', "...-" }, { 'w', ".--" }, { 'x', ".--" },
+            { 'y', "-.--" }, { 'z', "--.." },
+        };
     }
 }
